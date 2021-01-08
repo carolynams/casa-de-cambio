caacbb90bd2d0dda4f430de2afd3f0a0435fe219
Carolyna <Carolynacaarolmantovani@hotmail.com>
2021-01-08 00:48:59 -0300

    Incluisão da coleçaõd postman; Remoção de campos não utilizados na classe instimento

--- src/main/java/com/example/casadecambio/model/dto/InvestimentoDTO.java
+++ src/main/java/com/example/casadecambio/model/dto/InvestimentoDTO.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 import static java.math.RoundingMode.HALF_EVEN;
 
@@ -14,25 +13,18 @@ import static java.math.RoundingMode.HALF_EVEN;
 public class InvestimentoDTO {
 
     private Long id;
-    private String tipo;
     private BigDecimal valorInvestido;
     private BigDecimal quantidadeInvestida;
     private BigDecimal lucro;
-    private LocalDateTime dataDoInvestimento;
-    private BigDecimal cotacaoAtualBitcoin;
     private String cpf;
 
     public InvestimentoDTO() {
-        this.dataDoInvestimento = dataDoInvestimento;
     }
 
-    public InvestimentoDTO(String tipo, BigDecimal valorInvestido, BigDecimal quantidadeInvestida, BigDecimal lucro, LocalDateTime dataDoInvestimento, BigDecimal cotacaoAtualBitcoin, String cpf) {
-        this.tipo = tipo;
+    public InvestimentoDTO(BigDecimal valorInvestido, BigDecimal quantidadeInvestida, BigDecimal lucro, String cpf) {
         this.valorInvestido = valorInvestido.setScale(3, HALF_EVEN);
         this.quantidadeInvestida = quantidadeInvestida;
         this.lucro = lucro.setScale(3, HALF_EVEN);
-        this.dataDoInvestimento = dataDoInvestimento;
-        this.cotacaoAtualBitcoin = cotacaoAtualBitcoin.setScale(3, HALF_EVEN);
         this.cpf = cpf;
     }
 
